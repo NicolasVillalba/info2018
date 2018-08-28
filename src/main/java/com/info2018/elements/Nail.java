7433134ce54d757749e2532a0b4546d823980223
Nicolas Villalba <nerynicolas@gmail.com>
2018-08-28 11:40:57 -0300

    added setters & getter to Nail class

--- src/main/java/com/info2018/elements/Nail.java
+++ src/main/java/com/info2018/elements/Nail.java
@@ -1,7 +1,11 @@
 package com.info2018.elements;
 
 import com.info2018.generics.Countable;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Nail implements Countable {
 
     private Type type;
